23c1f8463aa768335240155cc4c548f0fe4a1f4f
srb4by <oliversanchezcasiano@gmail.com>
2024-01-16 14:42:46 +0100

    pasar la pagina a formato web

--- frontend/src/components/Page.jsx
+++ frontend/src/components/Page.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import "./Page.css";
+import axios from "axios";
+import { useEffect } from "react";
 
 function Page() {
+    const [hotels, setHotels] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/hotels")
+        .then((response) => setHotels(response.data))
+        .catch(error => console.error('Error al obtener datos del backend', error));      
+}, [])
+
+
     return (
         <div className="Page">
-            <div className="containerScreen">
+            
                 <span>Trooking</span>
-            </div>
+                {/*datos hoteles*/}
+                <ul>
+                    {hotels.map((hotel) => (
+                        <li key={hotel.id}>
+                            <div>
+                                <h3>{hotel.name}</h3>
+                                <p>{hotel.description}</p>
+                                <img src={hotel.photo_url} alt={hotel.name} />
+                                <p>Precio por noche: {hotel.price_per_night}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            
         </div>
     );
 }
