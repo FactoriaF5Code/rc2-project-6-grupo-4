ab34634b84520b7378c0afbfc24a14285f441e9a
Emilly3008 <151835514+Emilly3008@users.noreply.github.com>
2024-01-17 15:29:19 +0100

    alert

---
+++ frontend/src/components/AlertConfirm/AlertConfirm.jsx
@@ -0,0 +1,22 @@
+function AlertConfirm() {
+    return (
+        <div className="containerAlert">
+            <p className="alerta">Enhorabuena! Has reservado el hotel La Paca entre el 1 y el 7 de Febrero</p>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+    )
+
+}
+
+
+export default AlertConfirm
