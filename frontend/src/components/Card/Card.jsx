a9bbfb605f713bb653e4c96d911dd9a03d93f5d0
srb4by <oliversanchezcasiano@gmail.com>
2024-01-18 12:32:11 +0100

    añadir modal reservas

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,8 +1,27 @@
 import './Card.css';
-import { Button, IconButton } from '@mui/material';
+import { useState } from 'react';
+import { Button, IconButton, Dialog, DialogTitle, DialogContent, TextField } from '@mui/material';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
-export const Card = ({ ...hotel }) => {
+const Card = ({ ...hotel }) => {
+
+  const [isModalOpen, setIsModalOpen] = useState (false);
+  const [checkInDate, setCheckInDate] = useState ('');
+  const [checkOutDate, setCheckOutDate] = useState ('');
+
+  const handleReserverClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleReserveConfirm = () => {
+    console.log('Check-in Date:', checkInDate);
+    console.log('check-out Date:', checkOutDate);
+    setIsModalOpen(false);
+  };
 
   let priceColor = 'black';
   let msgPrice = '';
@@ -23,10 +42,33 @@ export const Card = ({ ...hotel }) => {
           <p className="price" style={{ color: priceColor }}>Precio por noche: {hotel.pricePerNight} €{msgPrice}</p>
         </div>
       <IconButton color="primary" aria-label="add to shopping cart">
-        <Button className="bton" size="large" variant="contained">Reservar!<AddShoppingCartIcon style={{ marginLeft: '1rem' }}/></Button>
+        <Button className="bton" size="large" variant="contained" onClick={handleReserverClick}>Reservar!<AddShoppingCartIcon style={{ marginLeft: '1rem' }}/></Button>
       </IconButton>
       </div>
+
+      <Dialog open={isModalOpen} onClose={handleModalClose}>
+        <DialogTitle>{`Reserva en${hotel.name}`}</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Fecha de Entrada"
+            type='date'
+            value={checkInDate}
+            onChange={(e) => setCheckInDate(e.target.value)}
+            InputLabelProps={{shrink: true}}
+            style={{marginBottom: '1rem' }}
+            />
+            <TextField
+            label="Fecha de salida"
+            type="date"
+            value={checkOutDate}
+            onChange={(e) => setCheckOutDate(e.target.value)}
+            InputLabelProps={{ shrink: true }}
+          />
+          <Button onClick={handleReserveConfirm}>Confirmar Reserva</Button>
+        </DialogContent>
+      </Dialog>
     </article>
   );
 };
 
+export { Card };
